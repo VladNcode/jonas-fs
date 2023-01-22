02ccc2dd91a2339b33c040113fcaa08e58863c95
VladNCode <vladncode@gmail.com>
2023-01-22 13:58:25 +0200

    vite

--- react/src/database/supabase.ts
+++ react/src/database/supabase.ts
@@ -2,4 +2,4 @@ import { createClient } from '@supabase/supabase-js';
 
 import { Database } from './database.types';
 
-export const supabase = createClient<Database>(import.meta.env.SUPABASE_URL, import.meta.env.SUPABASE_KEY);
+export const supabase = createClient<Database>(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_KEY);
